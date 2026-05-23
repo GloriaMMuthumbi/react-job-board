ca1701e5b30e89a5b44f2624b12e54aa1c82f8c1
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2026-05-23 22:21:40 +0300

    update:! add navigation to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
     return (
         <header className="bg-white border-b border-gray-200">
             <div className="max-w-7xl mx-auto px-6 lg:px-8 h-18 min-h-[72px] flex items-center justify-between">
-                <h1 className="text-[18px] font-semibold tracking-tight text-gray-900">
+                <Link className="text-[18px] font-semibold tracking-tight text-gray-900" to="/">
                     Job<span className="text-blue-600">Board</span>
-                </h1>
+                </Link>
             </div>
         </header>
     );
